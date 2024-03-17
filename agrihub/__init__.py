4b9bbe57bbd0811ba80813729987f0a28ab6d189
Aniket Gupta <107102866+ANIKEtguPta77@users.noreply.github.com>
2024-03-17 08:52:32 +0530

    Update __init__.py

--- agrihub/__init__.py
+++ agrihub/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 
 app=Flask(__name__)
 app.config['SECRET_KEY']='795e3ffa6939417dbd3dc4625a334330'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://user_5y15_user:SYmsYVJjPsbDWRhp5TWpxkBjbcP9naUB@dpg-cnp9d7nsc6pc73fqcvi0-a/user_5y15'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 db = SQLAlchemy(app)
 bcrypt=Bcrypt(app)
 login_manager=LoginManager(app)
